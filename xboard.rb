bfa6cec2ea55c0ed29764f18dea368b293c417d2
cmoyer <cmoyer>
2004-06-25 02:40:28 +0000

    redo

--- xboard.rb
+++ xboard.rb
@@ -1,7 +1,22 @@
 #!/usr/bin/env ruby
 
-load './board.rb'
-load './generator.rb'
+#load './board.rb'
+#load './generator.rb'
+load '/Users/cmoyer/Devel/rubyknight/board.rb'
+load '/Users/cmoyer/Devel/rubyknight/generator.rb'
+
+confirm = false
+$stdout.sync=true
+$stderr.sync=true
+
+def time_it label
+	yield
+end
+
+def logout msg
+	$stderr.print "Out:#{msg}\n"		
+	print "#{msg}\n"		
+end
 
 module Enumerable
 	def rand
@@ -22,58 +37,62 @@ def displayb b
 	end
 	print "\n"
 	if moves.size == 0
-		puts "Checkmate, you lose."
-		Kernel.exit 0
+		puts "RESULT 0-1 {Black Mates}"
 	end
-	print "Enter move [#{b.white_to_play? ? 'White' : 'Black'}]> "
+end
+
+def tocnote move
+	"#{RubyKnight::Board.position_to_coord move[0]}#{RubyKnight::Board.position_to_coord move[1]}"
 end
 
 def play b
 	moves = b.gen_legal_moves
 	if moves.size == 0
-		puts "Checkmate, you win!"	
-		Kernel.exit(0)
+		puts "RESULT 1-0 {White Mates}"
 	end
 	move = moves.rand
-	cnotation = "#{RubyKnight::Board.position_to_coord move[0]}#{RubyKnight::Board.position_to_coord move[1]}"
-	puts cnotation
+	cnotation = tocnote move
+	logout "move #{cnotation}"
 	b.cnotation_move cnotation
 end
 
 cplay = true
-puts 'go'
 b = RubyKnight::Board.new
-# displayb b
-#['e2e4' , 'e7e5' , 'd2d3'].each do |move|
-#['e2e4','d7d5','e4e5','f7f5'].each do |move|
 $stdin.each do |move|
 	move.strip!
-	if move =~ /!([^ ]*)[ ]*(.*)/
-		case $1
-			when "quit" then Kernel.exit 
-			when "undo" then b.undo 2
-			when "play" 
-				cplay = !cplay
-				play(b) if cplay
-			when "dump" 
-				File.open( $2, "w") { |f| f.write( b.dump) }
-				puts "dumped."
-			when "load" 
-				File.open( $2, "r") { |f| b.load( f.readlines.join) }
-				puts "loaded."
-			when "reset" 
-				b = RubyKnight::Board.new
-		end
-		# displayb b
-	else
-		begin
-			b.cnotation_move move
-			if cplay then play(b) end
-			# displayb b
-		rescue RubyKnight::IllegalMoveException
-			print "Enter a real move! #{$!.to_s}\n"
-			print "Enter move> "
-		end
+	$stderr.print "In :#{move}\n"
+	case move	
+		when "xboard" then 
+			logout ""
+			logout "tellics set f5 1=1"
+		when "confirm_moves"
+			confirm = true	
+			logout "Will now confirm moves."	
+		when /.{0,1}new/
+			b = RubyKnight::Board.new
+			logout "tellics set 1 RubyKnight"
+		when /^protover/ then 
+			logout "feature sigint=0 sigterm=0 ping=1 done=1"	
+		when /^ping\s+(.*)/ then 
+			logout "pong #{$1}"	
+		when /^st/ then 
+		when /^level/ then 
+		when /^time/ then 
+		when /^otim/ then 
+		when "hard" then 
+		when "random" then 
+		when /^accepted/ then 
+			# ignore	
+		else		
+			move.gsub!(/\?/, '')	
+			begin
+				b.cnotation_move move
+				logout "Legal move: #{move}" if confirm
+				if cplay then play(b) end
+				# displayb b
+			rescue RubyKnight::IllegalMoveException
+				logout "Illegal move: #{move}"
+			end
 	end
 end
 
